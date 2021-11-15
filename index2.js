6fcc54615b4f96e72e466b68b330d36e76875625
0101dd <aa09335665@gmail.com>
2021-11-15 11:12:17 +0800

    上傳

---
+++ index2.js
@@ -0,0 +1,78 @@
+import 'dotenv/config'
+import linebot from 'linebot'
+import axios from 'axios'
+// import cheerio from 'cheerio'
+
+const bot = linebot({
+  channelId: process.env.CHANNEL_ID,
+  channelSecret: process.env.CHANNEL_SECRET,
+  channelAccessToken: process.env.CHANNEL_ACCESS_TOKEN
+})
+
+bot.listen('/', process.env.PORT || 3000, () => {
+  console.log('機器人開始')
+})
+
+// bot.on('message', async (event) => {
+//   const txt = event.message.text
+//   console.log(txt)
+//   event.reply(txt)
+// })
+
+// live
+bot.on('message', async (event) => {
+  // const text = event.message.text
+  try {
+    const result = []
+    const { data } = await axios.get('https://api.holotools.app/v1/live')
+    let i = 0
+    for (i; i <= data.live.length; i++) {
+      const live = data.live[i].status
+      const type = event.message.text
+      console.log(type)
+      if (type === live) {
+        result.push('https://www.youtube.com/watch?v=' + data.live[i].yt_video_key)
+        if (result.length >= 5) {
+          break
+        }
+      } event.reply(result)
+    }
+    console.log(result)
+    if (result.length > 0) {
+      // event.reply(result)
+    } else {
+      event.reply('找不到')
+    }
+  } catch (error) {
+    console.log(error)
+    event.reply('error')
+  }
+})
+
+// past
+// bot.on('message', async (event) => {
+//   // const text = event.message.text
+//   try {
+//     const result = []
+//     const { data } = await axios.get('https://api.holotools.app/v1/live')
+//     let i = 0
+//     for (i; i <= data.ended.length; i++) {
+//       const past = data.ended[i].status
+//       const type = event.message.text
+//       console.log(type)
+//       if (type === past) {
+//         console.log(past)
+//         result.push('https://www.youtube.com/watch?v=' + data.ended[i].yt_video_key)
+//         if (result.length >= 5) {
+//           break
+//         }
+//         event.reply(result)
+//       } else {
+//         console.log('no')
+//         event.reply('no data')
+//       }
+//     }
+//   } catch (error) {
+//     console.log(error)
+//   }
+// })
